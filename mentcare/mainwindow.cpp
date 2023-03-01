1ac35e1e241e5f5062966e07f4779af363f68754
Xiang Cui <xc1008@nyu.edu>
2023-02-28 19:50:58 -0500

    Sprint 2 commit

--- mentcare/mainwindow.cpp
+++ mentcare/mainwindow.cpp
@@ -1,16 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+
+#include <QString>
 
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
+
     ui->setupUi(this);
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("../mentcare.sqlite");
+    userEmail="";
+    userRole="";
 }
 
 MainWindow::~MainWindow()
 {
+    db.close();
     delete ui;
 }
 
@@ -91,8 +101,7 @@ void MainWindow::on_patientsPage_exit_clicked()
 void MainWindow::on_stackedWidget_currentChanged(int arg1)
 {
     if (arg1 == 0){
-        QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-        db.setDatabaseName("../mentcare.sqlite");
+        //QSqlDatabase
         if (!db.open()){
             QMessageBox::information(this, "Connection", "Unable to Load Database");
             db.close();
@@ -106,8 +115,135 @@ void MainWindow::on_stackedWidget_currentChanged(int arg1)
             } else {
                  QMessageBox::information(this, "Connection", "Unable to Query Database");
             }
-            db.close();
+
         }
     }
+}
+
+
+// need to remove, just to make the code compile
+
+void MainWindow::on_pushButton_4_clicked() {
+
+}
+
+void MainWindow::on_pushButton_3_clicked() {
+
+}
+
+void MainWindow::on_LoginButton_clicked() {
+
+}
+
+// code above needs to remove, just to make the code compile
+
+
+// navigate from main page to loing page
+void MainWindow::on_HomePage_ToLoginPage_Button_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->LoginPage);
+    ui->LoginPage_LineEdit_Login_Password->clear();
+    ui->LoginPage_LineEdit_Login_Email->clear();
+}
+
+// handle login logic
+void MainWindow::on_LoginPage_LoginButton_clicked()
+{
+    QString email = ui->LoginPage_LineEdit_Login_Email->text();
+    QString password = ui->LoginPage_LineEdit_Login_Password->text();
+
+    bool validUser = loginAccess(email, password);
+
+    if (validUser) {
+        QMessageBox::information(this, "Login", "Successful!");
+        ui->stackedWidget->setCurrentWidget(ui->HomePage);
+    } else {
+        QMessageBox::information(this, "Login", "Login Failed, please try again!");
+    }
+}
+
+bool MainWindow::loginAccess(QString email, QString password) {
+    if (!db.open()) {
+        //maybe this should be moved to a UI type function?
+        QMessageBox::information(this, "Connection", "Unable to Load Database");
+        db.close();
+        return false;
+    } else {
+        QSqlQuery query;
+        query.prepare("SELECT * FROM users WHERE users.email=:loginEmail AND users.password=:loginPassword");
+        query.bindValue(":loginEmail", email);
+        query.bindValue(":loginPassword", password);
+        if (query.exec() && query.next()) {
+            userEmail = email;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// navigate from login page to main page
+// need to remove this button and code, the login button will check credentials and progress the user
+void MainWindow::on_LoginPage_BackToMainButton_clicked()
+{
+    //ui->stackedWidget->setCurrentWidget(ui->HomePage);
+}
+
+
+
+
+
+void MainWindow::on_HomePage_ToRegisterPage_Button_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->RegisterPage);
+}
+
+
+void MainWindow::on_RegisterPage_LoginButton_clicked()
+{
+    QString email = ui->RegisterPage_LineEdit_Login_Email->text();
+    QString pass1 = ui->RegisterPage_LineEdit_Login_Password->text();
+    QString pass2 = ui->RegisterPage_LineEdit_Login_Password_2->text();
+    if (pass1!=pass2) {
+        QMessageBox::information(this, "Registration", "Passwords do not match");
+        std::cout << pass1.toStdString() << std::endl << pass2.toStdString();
+    } else {
+        bool userAdded = newUser(email, pass1);
+        if (userAdded) {
+            QMessageBox::information(this, "Registration", "New user added!");
+        } else {
+            QMessageBox::information(this, "Registration", "Unable to add new user");
+        }
+    }
+
+}
+
+bool MainWindow::newUser(QString email, QString password) {
+    if (!db.open()) {
+        //maybe this should be moved to a UI type function?
+        QMessageBox::information(this, "Connection", "Unable to Load Database");
+        db.close();
+        return false;
+    } else {
+        QSqlQuery query;
+        query.prepare("INSERT INTO users (email, password)"
+                      "VALUES (:email, :password)");
+        query.bindValue(":email", email);
+        query.bindValue(":password", password);
+        if (query.exec()) {// && query.next()) {
+            //userEmail = email;
+            std::cout << "success: " << query.lastQuery().toStdString() << std::endl;
+            return true;
+        } else {
+            std::cout << "fail: " << query.lastQuery().toStdString() << std::endl;
+            return false;
+        }
+    }
+}
+
+
+void MainWindow::on_RegisterPage_LoginButton_2_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->HomePage);
 }
 
--- mentcare/mainwindow.h
+++ mentcare/mainwindow.h
@@ -6,9 +6,20 @@
 #include <QSqlDatabase>
 #include <QMessageBox>
 #include <QSqlQuery>
+#include <Qmap>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+    class MainWindow;
+
+    const QMap<QString, QString> fixedLogin = {
+        {QString("ua2047@nyu.edu"), QString("ushan")},
+        {QString("xc1008@nyu.edu"), QString("jeff")},
+        {QString("kod2003@nyu.edu"), QString("kobe")},
+        {QString("kc4302@nyu.edu"), QString("kenneth")},
+        {QString("admin"), QString("pass")}
+    };
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -18,6 +29,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    bool loginAccess(QString email, QString password);
+    bool newUser(QString email, QString password);
 
 private slots:
 
@@ -33,7 +47,30 @@ private slots:
 
     void on_stackedWidget_currentChanged(int arg1);
 
+    void on_pushButton_4_clicked();
+
+    void on_LoginButton_clicked();
+
+    void on_LoginPage_LoginButton_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_HomePage_ToLoginPage_Button_clicked();
+
+    void on_LoginPage_BackToMainButton_clicked();
+
+    void on_HomePage_ToRegisterPage_Button_clicked();
+
+    void on_RegisterPage_LoginButton_clicked();
+
+    void on_RegisterPage_LoginButton_2_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    QString userEmail;
+    QString userRole;
+    QSqlDatabase db;
+
 };
 #endif // MAINWINDOW_H
